40ca2e95c282eac8cabc7016e02b0bfa830af8f8
Nick <nick@Nicks-MacBook-Pro.local>
2019-01-31 21:24:13 +0100

    Refactored straight ahead with lambdas as strategies, without domain model, just to see where this would end

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,62 +1,60 @@
 package com.gildedrose;
 
+import java.util.function.Function;
+
+import static java.lang.Math.max;
+import static java.lang.Math.min;
+import static java.util.Arrays.stream;
+
 class GildedRose {
     Item[] items;
 
-    public GildedRose(Item[] items) {
-        this.items = items;
-    }
+    public GildedRose(Item[] items) { this.items = items; }
 
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
-
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
-
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
-            }
-        }
+        stream(items).forEach(this::updateQuality);
     }
+
+    private void updateQuality(Item item) {
+        adjustSellIn(item);
+        adjustQuality(item);
+    }
+
+    private Item adjustSellIn(Item item) {
+        if (hasExpiryDate(item)) {
+            item.sellIn -= 1;
+        }
+        return item;
+    }
+
+    private void adjustQuality(Item item) {
+        item.quality = capped(item, qualityAdjustmentFor(item));
+    }
+
+    private Function<Item, Integer> qualityAdjustmentFor(Item item) {
+        if (isSulphuras(item))     return sulphur -> 80;
+        if (isBackstagePass(item)) return pass    -> pass.quality = sellDatePassed(pass) ? 0 : pass.quality + incrementForBackPass(pass);
+        if (isBrie(item))          return brie    -> brie.quality  + 1 * degradationVelocity(brie); //mimic bug in the source code
+        else                       return other   -> other.quality - 1 * degradationVelocity(other);
+    }
+
+    private int capped(Item item, Function<Item, Integer> qualityUpdateAlgorithm) {
+        return isSulphuras(item) ? 80 : max(min(qualityUpdateAlgorithm.apply(item), 50), 0);
+    }
+
+    private int incrementForBackPass(Item pass) {
+        int increment = 1;
+        if (pass.sellIn < 10) increment++;
+        if (pass.sellIn < 5)  increment++;
+        return increment;
+    }
+
+    private boolean sellDatePassed(Item item)  { return item.sellIn < 0; }
+    private boolean hasExpiryDate(Item item)   { return !isSulphuras(item); }
+    private int degradationVelocity(Item item) { return sellDatePassed(item) ? 2 : 1;}
+
+    private boolean isSulphuras(Item item)     { return item.name.equals("Sulfuras, Hand of Ragnaros");               }
+    private boolean isBackstagePass(Item item) { return item.name.equals("Backstage passes to a TAFKAL80ETC concert");}
+    private boolean isBrie(Item item)          { return item.name.equals("Aged Brie");                                }
+
 }
